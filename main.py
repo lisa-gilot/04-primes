cab59596b90550e9791932eab4a461cc90c3515e
lisa-gilot <lisa.gilot@edu.esiee.fr>
2025-09-22 12:01:32 +0000

    exercice nombre premier

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+"""
+Module pour afficher les nombres premiers inférieurs à 100.
+"""
+
 from math import sqrt
 
-#### Fonction secondaire
-
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
-
-#### Fonction principale
+    """
+    Détermine si p (le nombre à tester) est un nombre est premier.
+    Renvoie un booléen indiquant True si p est premier
+    """
+    if p < 2:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
-
+    """
+    Affiche tous les nombres premiers inférieurs à 100.
+    """
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
-    print()
+        print()
 
 
 if __name__ == "__main__":
